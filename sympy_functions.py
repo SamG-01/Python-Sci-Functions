6589bdea918ddc8939b6bcf6cbfc587c536a2139
SamG-01 <103982805+SamG-01@users.noreply.github.com>
2022-12-20 01:06:59 -0500

    Update sympy_functions.py
    Removes bad functions

--- sympy_functions.py
+++ sympy_functions.py
@@ -1,14 +1,16 @@
 import sympy as sp
-from sympy.physics import units
+from sympy.physics.units import convert_to
 
 def tex(expr):
-    """Prints an expression in LaTeX form"""
+    """Prints an expression in LaTeX form."""
     print(sp.latex(expr))
 
-def unit_value(expr, unit=1):
-    """Converts an expression to the given unit(s), and outputs the scaling factor associated with the resultant quantity."""
-    expr = units.convert_to(expr, unit)
-    return expr.as_coeff_Mul()[0]
+def unit_value(quantity, units=1, return_float=False):
+    """Returns the numerical value of a quantity in the given units."""
+    n = convert_to(quantity, units)/units
+    if return_float:
+        n = float(n)
+    return n
 
 def normalize(f, x, key, just_constant=False):
     """Normalizes a function f(x) such that evaluating the key function returns one. The normalization constant C is assumed to be positive.
@@ -32,24 +34,3 @@ def f_inner(f, g, bounds):
 def f_norm(f, bounds):
     """Computes the L^2 norm of f over the bounds."""
     return sp.sqrt(f_inner(f, f, bounds))
-
-def convert_function(expr, variables):
-    """Transforms an expression in terms of some variables into a function of said variables.
-    
-    Examples
-    ===========
-    >>> f = convert_function(x**2 - y**3, [x, y])
-    >>> f(x, y)
-    x**2 - y**3
-    >>> f(z, 2)
-    z**2 - 8
-    >>> f(3, t)
-    9 - t**3
-    """
-    def f(*new_vars):
-        nonlocal expr
-        old_len, new_len = len(variables), len(new_vars)
-        if old_len != new_len:
-            raise TypeError(f"Function takes {old_len} positional arguments but {new_len} were given")
-        return expr.subs({x: x_new for x, x_new in zip(variables, new_vars)})
-    return f
